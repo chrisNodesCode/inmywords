c02804d1ff9cd4e51e8b367753627246912d6fa7
Chris <120611463+chrisNodesCode@users.noreply.github.com>
2025-08-13 01:52:58 -0500

    Add fallbacks for group card colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import { SessionProvider } from 'next-auth/react';
+import { ConfigProvider } from 'antd';
 import '../src/styles/main.css';
 import 'antd/dist/reset.css';
 import ThemeProvider from '../src/components/ThemeProvider';
@@ -8,9 +9,11 @@ export default function App(props) {
   const { Component, pageProps } = props;
   return (
     <SessionProvider session={pageProps.session}>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ConfigProvider theme={{ cssVar: true }}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ConfigProvider>
     </SessionProvider>
   );
 }
